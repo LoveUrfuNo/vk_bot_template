8f7be57364704bcbd25668321531db07f3f877e9
Andrey Kostyuchenko <andreykostyuchenko87@gmail.com>
2018-12-05 23:17:28 +0500

    Delete messages.py

--- bot/messages.py
+++
@@ -1,7 +0,0 @@
-NOT_SUBSCRIBED_MESSAGE = 'Понравилось? Так подпишись на меня😊 vk.com/bot_bitch'
-GROUP_JOIN_MESSAGE = "Сообщения при подписке"
-GROUP_LEAVE_MESSAGE = "Сообщение при отписке"
-GROUP_OFFICERS_EDIT_MESSAGE = 'Состав администрации сообщества был изменен'
-
-REDUNDANT_ANSWER_EXCEPTION_MESSAGE = 'Bot answer is not needed at the moment'
-VK_SECRET_KEY_EXCEPTION_MESSAGE = 'Secret key from current request is not equal correct secret key'
